6b59423794673614b17549f94f3bf04830dd525d
todoroki24 <mayanklovevanshi2005@gmail.com>
2025-12-07 10:01:37 +0530

    Day-2 progress

--- OwnSetterGetter.java
+++ OwnSetterGetter.java
@@ -5,7 +5,7 @@ import java.lang.Class;
 public class OwnSetterGetter
 {
 
-public String argumentMaker(Field f)
+public static String argumentMaker(Field f)
 {
 StringBuffer argumentTemplate = new StringBuffer();
 
@@ -23,6 +23,34 @@ if (s == null || s.isEmpty()) return s;
 return s.substring(0, 1).toUpperCase() + s.substring(1);
 }
 
+public String setterGenerator(Field f[])
+{
+StringBuffer setterTemplate = new StringBuffer();
+for(int i=0;i<f.length;i++)
+{
+String fieldName = f[i].getName();
+String fieldDataType = f[i].getType().getSimpleName();
+setterTemplate.append("public void ");
+setterTemplate.append("set"+capitalize(fieldName));
+setterTemplate.append(argumentMaker(f[i]));
+setterTemplate.append("\n{\n");
+setterTemplate.append("this."+fieldName+" = "+fieldName+";\n}");
+setterTemplate.append("\n");
+}
+return setterTemplate.toString();
+}
+
+public String getterGenerator(Field f[])
+{
+StringBuffer getterTemplate = new StringBuffer();
+for(int i=0;i<f.length;i++)
+{
+String fieldName = f[i].getName();
+String fieldDataType = f[i].getType().getSimpleName();
+getterTemplate.append("public "+fieldDataType+" get"+capitalize(fieldName)+"()\n{\n"+"return this."+fieldName+";\n}\n");
+}
+return getterTemplate.toString();
+}
 
 public static void main(String args[]) throws Exception
 {
@@ -36,28 +64,7 @@ String className = args[0];
 Class c = Class.forName(className);
 Field f[] = c.getDeclaredFields();
 
-StringBuffer setterTemplate = new StringBuffer();
-StringBuffer getterTemplate = new StringBuffer();
-
-for(int i=0;i<f.length;i++)
-{
-String fieldName = f[i].getName();
-String fieldDataType = f[i].getType().getSimpleName();
-setterTemplate.append("public void ");
-setterTemplate.append("set"+capitalize(fieldName));
-setterTemplate.append(mk.argumentMaker(f[i]));
-setterTemplate.append("\n{\n");
-setterTemplate.append("this."+fieldName+" = "+fieldName+";\n}");
-setterTemplate.append("\n");
-}
-
-for(int i=0;i<f.length;i++)
-{
-String fieldName = f[i].getName();
-String fieldDataType = f[i].getType().getSimpleName();
-getterTemplate.append("public "+fieldDataType+" get"+capitalize(fieldName)+"()\n{\n"+"return this."+fieldName+";\n}\n");
-}
-System.out.println(getterTemplate.toString());
-System.out.println(setterTemplate.toString());
+System.out.println(mk.getterGenerator(f));
+System.out.println(mk.setterGenerator(f));
 }
 }
